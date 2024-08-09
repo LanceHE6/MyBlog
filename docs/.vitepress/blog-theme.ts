457be8440e48dd940d08d91fd137bbe4e3e42c40
LanceHE6 <2765543491@qq.com>
2024-08-09 17:37:31 +0800

    chore:修改搜索框样式

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -23,6 +23,12 @@ const blogTheme = getThemeConfig({
   // 搜索
   // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
   // search: false,
+  search :{
+    btnPlaceholder: '搜索',
+    placeholder: '搜索文章',
+    emptyText: '无匹配结果',
+    heading: '总共: {{searchResult}} 个搜索结果.'
+  },
 
   // markdown 图表支持（会增加一定的构建耗时）
   mermaid: true,
